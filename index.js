c50b1bed191eaa3407a79a7103dd59b2fb5e7e24
adrioro <artistsvillage@me.com>
2019-01-24 09:28:11 -0500

    Fifth commit

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ $("#buttonId").click(function () {
     $("#myText").focus();
     $("#myText").css("border", "3px solid blue");
 });
+
+$("#validate").click(function () {
+    //validate info from $("#name");
+    if ($("#name").val() != "") {
+        $(this).submit();
+    } else {
+        $("form").after("<br>Text was not entered.We can't validate.");
+    }
+});
